45698baf15e5a79a69a5223009c0707bc5f0bf7f
Juan Guana <juanc.guana@gmail.com>
2021-05-21 22:35:38 -0500

    feat: get data from api

--- src/components/CarruselItem.jsx
+++ src/components/CarruselItem.jsx
@@ -3,20 +3,20 @@ import '../assets/styles/components/CarruselItem.scss';
 import playIcon from '../assets/static/play-icon.png';
 import plusIcon from '../assets/static/plus-icon.png';
 
-const CarruselItem = ({ children }) => (
+const CarruselItem = ({ cover, title, year, contentRating, duration }) => (
   <div className='carousel-item'>
     <img
       className='carousel-item__image'
-      src='https://images.pexels.com/photos/3526020/pexels-photo-3526020.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260'
-      alt='Chuwaca'
+      src={cover}
+      alt={title}
     />
     <div className='carousel-item__details'>
       <div>
         <img src={playIcon} alt='Play icon' />
         <img src={plusIcon} alt='Plus Icon' />
       </div>
-      <p className='carousel-item__details--title'>Titulo descriptivo</p>
-      <p className='carousel-item__details--subtitle'>2019 16+ 114 minutos</p>
+      <p className='carousel-item__details--title'>{title}</p>
+      <p className='carousel-item__details--subtitle'>{`${year} ${contentRating} ${duration}`}</p>
     </div>
   </div>
 );
--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -6,32 +6,43 @@ import Categories from '../components/Categories';
 import Carrusel from '../components/Carrucel';
 import CarruselItem from '../components/CarruselItem';
 import Footer from '../components/Footer';
+import useInitialState from '../hooks/useInitialState';
 
-const App = () => (
-  <div className='App'>
-    <Header />
-    <Search />
-    <Categories title='Mi Lista'>
-      <Carrusel>
-        <CarruselItem />
-        <CarruselItem />
-        <CarruselItem />
-        <CarruselItem />
-      </Carrusel>
-    </Categories>
-    <Categories title='Tendencias'>
-      <Carrusel>
-        <CarruselItem />
-        <CarruselItem />
-      </Carrusel>
-    </Categories>
-    <Categories title='Originales'>
-      <Carrusel>
-        <CarruselItem />
-      </Carrusel>
-    </Categories>
-    <Footer />
-  </div>
-);
+const API = 'http://localhost:3000/initalState';
 
+const App = () => {
+  const videos = useInitialState(API);
+
+  return (
+    <div className='App'>
+      <Header />
+      <Search />
+      {videos.mylist.length > 0 && (
+        <Categories title='Mi Lista'>
+          <Carrusel>
+            {videos.mylist.map((item) => (
+              <CarruselItem key={item.id} {...item} />
+            ))}
+          </Carrusel>
+        </Categories>
+      )}
+
+      <Categories title='Tendencias'>
+        <Carrusel>
+          {videos.trends.map((item) => (
+            <CarruselItem key={item.id} {...item} />
+          ))}
+        </Carrusel>
+      </Categories>
+      <Categories title='Originales'>
+        <Carrusel>
+          {videos.originals.map((item) => (
+            <CarruselItem key={item.id} {...item} />
+          ))}
+        </Carrusel>
+      </Categories>
+      <Footer />
+    </div>
+  );
+};
 export default App;
